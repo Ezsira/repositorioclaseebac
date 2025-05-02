5842e7b1816a8019471d0030de3812ed7be749c4
Ezsira <miocidcampeador1@hotmail.com>
2025-05-02 11:50:09 -0600

    Modulo 5
    -se hizo el script hola mundo y se coloco e la main camera
    -se utilizaron las diferentes funciones de Log

--- ProyectoinicialEBAC/Assets/Scripts/HolaMundo.cs
+++ ProyectoinicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
         print("Algo Paso");
-        Debug.Log("Algo Paso");
+        
         Debug.LogWarning("Algo Salio Medianamente Mal");
         Debug.LogError("Algo Salio Mu Mal");
     }
@@ -16,6 +18,27 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // x = x + 1;
+        // Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde late update"); 
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("El objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitado");
     }
 }
