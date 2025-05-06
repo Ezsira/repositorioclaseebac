f30bb02fa49b9afdec7a8a56679859db8c59b965
Rodrigo <miocidcampeador1@hotmail.com>
2025-05-06 12:21:43 -0600

    Color aleatorio cubo
    Se creo codigo para hacer que el prefab de cubo cambiara de color al azar al ser arrastrado

---
+++ ProyectoinicialEBAC/Assets/Modulo6/Scripts/RandomColor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    public GameObject PrefabCubo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCubo);
+        Color c = new Color(Random.value,Random.value,Random.value);
+        tempGameObject.GetComponent<MeshRenderer>().material.color = c;
+
+    }
+}
